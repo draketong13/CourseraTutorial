d1f24fc836434249c060a6c050a1ae8d18999dce
JemmyJey <56714262+draketong13@users.noreply.github.com>
2023-07-12 23:06:04 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Exercise 2: Create markdown cell with title of the notebook
+# # Data Science Tools and Ecosystem
+
+# Exercise 3: Create a markdown cell for an introduction
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Exercise 4: Create a markdown cell to list data science languages
+# ## Some of the popular languages that Data Scienties use are:
+# 1. Python
+# 2. R language
+# 3. SQL
+
+# Exercise 5: Create a markdown cell to list data science libraries
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 1. Scientific Computing Libraries in Python 
+# 2. Visualization Libraries in Python
+# 3. High-Level-Machine Learning and Deep Learning
+# 4. Deep Learning Libraries in Python
+# 5. Libraries used in other languages
+
+# Exercise 6: Create a markdown cell with a table of Data Science tools
+# ## Data Science Tools
+# |DATA SCIENCE TOOLS|Example|
+# |--|--|
+# |Data Management|MySQL, MongoDB|
+# |Data Integration and Transformation|Apache AirFlow, Apache SparkSQL|
+# |Data Visualization|Pixie Dust, Hue|
+# |Model Deployment|Apache PredictionIO, TensorFlow lite, Tensorflow dot JS|
+# |Model Monitoring|ModelDB, IBM AI Fairness 360|
+# |Code Asset Management|Git, GitHub, Bitbucket|
+# |Data Asset Management|Apache Atlas, Kylo|
+
+# Exercise 7: Create a markdown cell introducing arithmetic expression examples
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# |Operators|Syntax|Functioning|
+# |--|--|--|
+# |+|x+y|Additon|
+# |-|x-y|Subtraction|
+# |* |x* y|Multiplication|
+# |/|x/y|Division|
+# |//|x//y|Quotient|
+# |%|x%y|Remainder|
+# |** |x** y|Exponentiation|
+
+# Exercise 8: Create a code cell to multiply and add numbers
+
+# In[7]:
+
+
+x = (3*4)+5
+print(x)
+
+
+# Exercise 9 - Create a code cell to convert minutes to hours
+
+# In[68]:
+
+
+min_to_hour = 60
+hours = time // min_to_hour
+time %= min_to_hour
+minutes = time
+time = 200
+print("%d minutes is equal to %d hours and %d minutes" % (time, hours, minutes))
+
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+
+# ## Objectives: 
+# ### List popular languages for Data Science
+# - Python
+# - R language
+# - SQL
+
+# Exercise 11 - Create a markdown cell to indicate the Author's name
+
+# ## Author
+# Jeremy Tong
+# 
